44d7856c11974588f548797099bc95763fef0db1
Muhammad Zeeshan Asghar <93910791+muhammad-zee@users.noreply.github.com>
2025-11-07 01:43:13 +0500

    changes

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PublicHeaderComponent } from '../public-header/public-header.component';
-import { PublicJoinComponent } from '../public-join/public-join.component';
-import { PublicRoomComponent } from '../public-room/public-room.component';
-import { PublicQuizResultComponent } from '../public-quiz-result/public-quiz-result.component';
+import { PublicHeaderComponent } from './public-header/public-header.component';
+import { PublicJoinComponent } from './public-join/public-join.component';
+import { PublicRoomComponent } from './public-room/public-room.component';
+import { PublicQuizResultComponent } from './public-quiz-result/public-quiz-result.component';
 // import { PublicLayoutComponent } from './public-layout.component';
 // import { HomeComponent } from './pages/home/home.component';
 
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -6,10 +6,10 @@ import { PublicRoutingModule } from './public-routing.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { SharedModule } from '../components/share.module';
-import { PublicJoinComponent } from '../public-join/public-join.component';
-import { PublicHeaderComponent } from '../public-header/public-header.component';
-import { PublicRoomComponent } from '../public-room/public-room.component';
-import { PublicQuizResultComponent } from '../public-quiz-result/public-quiz-result.component';
+import { PublicJoinComponent } from './public-join/public-join.component';
+import { PublicHeaderComponent } from './public-header/public-header.component';
+import { PublicRoomComponent } from './public-room/public-room.component';
+import { PublicQuizResultComponent } from './public-quiz-result/public-quiz-result.component';
 
 
 
